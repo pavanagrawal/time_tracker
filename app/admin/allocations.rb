10499ab1a8764d34469ca629ef11f46a4c4ff925
Pavan Agrawal <pavan.agrawala@gmail.com>
2017-08-09 12:52:42 +0530

    Bug Fixes - 2

--- app/admin/allocations.rb
+++ app/admin/allocations.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Allocation do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+  permit_params :project_id, :user_id, :start_date, :end_date
 #
 # or
 #
